bf97815928be6f6e5cc3de96decaeb95d9e61691
karya16 <kartikarya1210@gmail.com>
2026-03-09 17:27:56 +0530

    Updated API base URL to production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = " http://127.0.0.1:8000/api/";
+const BASE_URL = " https://hrms-lite-tfqr.onrender.comapi/";
 
 export const api = async (endpoint, options = {}) => {
   const res = await fetch(BASE_URL + endpoint, {
